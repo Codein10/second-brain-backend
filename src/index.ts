e46d61e2696124681d293c91f26d2b355bcc2a86
Codein10 <dineshkum56p@gmail.com>
2026-05-17 15:56:46 +0530

    cord issu resolved

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL|| "http://localhost:5173",
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 const JWT_SECRET = process.env.JWT_SECRET;
